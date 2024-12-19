f2ca22e74d07e5a2266869a5e295b02916277e94
roju <10701214+roju@users.noreply.github.com>
2024-12-19 15:49:57 -0800

    Update radxa-zero3.sh

--- config/boards/radxa-zero3.sh
+++ config/boards/radxa-zero3.sh
@@ -24,9 +24,12 @@ function config_image_hook__radxa-zero3() {
 
         chroot $chroot_dir apt-get install -y software-properties-common
         chroot $chroot_dir add-apt-repository ppa:jjriek/rockchip
-
+        chroot $chroot_dir apt-get update
+        chroot $chroot_dir apt-get -y install dkms
+        echo "kernelsourcedir=/usr/src/linux-headers-6.1.0-1025-rockchip" > "${rootfs}/etc/dkms/framework.conf"
+        
         # Install AIC8800 SDIO WiFi and Bluetooth DKMS
-        chroot "${rootfs}" apt-get -y install dkms aic8800-firmware aic8800-sdio-dkms
+        chroot "${rootfs}" apt-get -y install aic8800-firmware aic8800-sdio-dkms
 
         # shellcheck disable=SC2016
         echo 'SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="88:00:*", NAME="$ENV{ID_NET_SLOT}"' > "${rootfs}/etc/udev/rules.d/99-radxa-aic8800.rules"
