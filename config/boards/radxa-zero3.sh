96c26b0a669619d9c594f5732775c6bfb4c2f77e
roju <10701214+roju@users.noreply.github.com>
2024-12-19 22:07:40 -0800

    Update radxa-zero3.sh

--- config/boards/radxa-zero3.sh
+++ config/boards/radxa-zero3.sh
@@ -22,18 +22,18 @@ function config_image_hook__radxa-zero3() {
             echo "blacklist aic8800_btlpm"
         ) > "${rootfs}/etc/modprobe.d/aic8800.conf"
 
-        # chroot "${rootfs}" apt-get install -y software-properties-common mtd-utils
-        # chroot "${rootfs}" add-apt-repository ppa:jjriek/rockchip
+        chroot "${rootfs}" apt-get install -y software-properties-common mtd-utils
+        chroot "${rootfs}" add-apt-repository ppa:jjriek/rockchip
         # uname -r
         # chroot "${rootfs}" uname -r
-        # chroot "${rootfs}" apt-mark hold linux-headers-6.8.0-31 linux-headers-6.8.0-31-generic linux-headers-generic
+        chroot "${rootfs}" apt-mark hold linux-headers-6.8.0-31 linux-headers-6.8.0-31-generic linux-headers-generic
         # chroot "${rootfs}" apt-mark showhold
         # chroot "${rootfs}" apt-get -o Debug::pkgProblemResolver=true -o Debug::pkgDPkgPM=true -y install dkms mtd-utils
         #echo "kernel_source_dir=/usr/src/linux-headers-6.1.0-1025-rockchip" > "${rootfs}/etc/dkms/framework.conf"
         #cat ${rootfs}/etc/dkms/framework.conf
         
         # Install AIC8800 SDIO WiFi and Bluetooth DKMS
-        chroot "${rootfs}" apt-get -y install mtd-utils dkms aic8800-firmware aic8800-sdio-dkms
+        chroot "${rootfs}" apt-get -y install dkms aic8800-firmware aic8800-sdio-dkms
         # chroot "${rootfs}" /bin/bash -c 'kernel_source_dir=/usr/src/linux-headers-6.1.0-1025-rockchip; echo ${kernel_source_dir}; apt-get -y install aic8800-firmware aic8800-sdio-dkms'
 
         # shellcheck disable=SC2016
